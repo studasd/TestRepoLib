9b1aef6ee3d23fa5604727d3f2cdfeb097dc136f
fuser85 <fuser85@myoffice365.site>
2020-04-20 11:47:20 +0300

    Добавьте файлы проекта.

---
+++ TestRepoLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRepoLib
+{
+    public class Class1
+    {
+        public string MyDLL = "Библиотека сервиса YA";
+    }
+}
