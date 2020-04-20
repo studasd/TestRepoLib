6359722dc8c02af029373e80e1277c0a2562550f
fuser85 <fuser85@myoffice365.site>
2020-04-20 18:59:08 +0300

    aerbeb

--- TestRepoLib/NewClass.cs
+++ TestRepoLib/NewClass.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace TestRepoLib
 {
-	class NewClass
+	public class NewClass
 	{
 		public string newClass = "Новый класс";
 		public string newClass2 = "Новый класс2";
+		public string newClass3 = "Новый класс3";
 	}
 }
